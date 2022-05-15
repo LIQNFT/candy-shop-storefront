519d17bebf5e9db4e8077f839713e603f6ce7485
jdojade <jade.delima.oloroso@gmail.com>
2022-05-15 21:20:13 +0800

    update v2

--- src/views/Marketplace.tsx
+++ src/views/Marketplace.tsx
@@ -32,7 +32,7 @@ const Marketplace: React.FC = () => {
       <Stat
         candyShop={candyShopRef.current}
         title={'Marketplace'}
-        description={'Candy Shop is an open source on-chain protocol that empowers DAOs, NFT projects and anyone interested in creating an NFT marketplace to do so within minutes!'}
+        description={''}
         style={{ paddingBottom: 50 }}
       />
       <Orders
