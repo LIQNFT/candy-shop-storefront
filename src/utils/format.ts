f76df4d05ff4252b4da8bfde76ee927fbaea0757
jcp-qcu <jc.pablico07081998@gmail.com>
2022-05-16 22:55:38 +0800

    Updatee

--- src/utils/format.ts
+++
@@ -1,6 +0,0 @@
-// Format date
-export function formatDate(date: string | Date): string {
-  const data = new Date(date);
-
-  return `${data.getDate()}/${data.getMonth()}/${data.getFullYear() % 100} ${data.getHours()}:${data.getMinutes()}`;
-}
