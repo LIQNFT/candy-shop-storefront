b69c8fec8240dd9c30871fd583231c1b8e9e5ef8
jdojade <jade.delima.oloroso@gmail.com>
2022-06-10 15:16:04 +0800

    update

--- src/views/Cards.js
+++ src/views/Cards.js
@@ -24,6 +24,28 @@ function Cards() {
             </div>
           </div>
         </Link>
+        
+        <Link to="/SLC" className="href">
+          <div class="card-container container-fluid">
+            <div class="upper-container">
+            <img src="/Structural-Legacies.png" className="cards-banner"/>
+              <div class="image-container">
+              <img src="/Homeqube-logo-black_small 1.svg" />
+              </div>
+            </div>
+            <div class="lower-container">
+              <div>
+                <h3>Structural Legacies with Cantilever</h3>
+                {/* <h4>2ND TITLE</h4> */}
+              </div>
+              <div>
+              <p className="SL-Des">
+              Introducing Homeqube’s Structural Legacy System. Each Structural Configuration is unique. We generated 50 unique combinations (with Cantilevers) from our Systems Architecture Platform (SAP). This Legacy System can be used to build limitless design configurations for flat topographies and up to 3 story housing, allowing open architecture. Our composite mix is more sustainable, weather resilient, and emits much less carbon than reinforced concrete/steel, and can be ergonomically assembled at site.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
 
         <Link to="/home-design" className="href">
           <div class="card-container container-fluid">
@@ -47,27 +69,7 @@ function Cards() {
           </div>
         </Link>
 
-        <Link to="/SLC" className="href">
-          <div class="card-container container-fluid">
-            <div class="upper-container">
-            <img src="/Structural-Legacies.png" className="cards-banner"/>
-              <div class="image-container">
-              <img src="/Homeqube-logo-black_small 1.svg" />
-              </div>
-            </div>
-            <div class="lower-container">
-              <div>
-                <h3>Structural Legacies with Cantilever</h3>
-                {/* <h4>2ND TITLE</h4> */}
-              </div>
-              <div>
-              <p className="SL-Des">
-              Introducing Homeqube’s Structural Legacy System. Each Structural Configuration is unique. We generated 50 unique combinations (with Cantilevers) from our Systems Architecture Platform (SAP). This Legacy System can be used to build limitless design configurations for flat topographies and up to 3 story housing, allowing open architecture. Our composite mix is more sustainable, weather resilient, and emits much less carbon than reinforced concrete/steel, and can be ergonomically assembled at site.
-                </p>
-              </div>
-            </div>
-          </div>
-        </Link>
+
       </div>
       <div class="display2">
         <Link to="/OSAP" className="href">
