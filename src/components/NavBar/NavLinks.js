78633f5cf474066899a9e7cb2e78a70ec9875c9e
jdojade <jade.delima.oloroso@gmail.com>
2022-06-17 21:28:35 +0800

    update navbar

--- src/components/NavBar/NavLinks.js
+++ src/components/NavBar/NavLinks.js
@@ -1,68 +1,184 @@
-import React from 'react'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { NavLink } from 'react-router-dom'
-import { useAnchorWallet } from '@solana/wallet-adapter-react'
-import MenuItem from '@material-ui/core/MenuItem'
-import MenuList from '@material-ui/core/MenuList'
-import ClickAwayListener from '@material-ui/core/ClickAwayListener'
-import CurrencyToggle from '../CurrencyToggle'
-import Paper from '@material-ui/core/Paper'
-import Popper from '@material-ui/core/Popper'
-import styled from 'styled-components'
-import { useState } from 'react'
+import React from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { NavLink } from "react-router-dom";
+import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import CurrencyToggle from "../CurrencyToggle";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import styled from "styled-components";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const NavLinks = (props) =>{
+const DropDownContainer = styled("li")`
+`;
 
-    const [active, setActive] = useState("navItem1");
-    return(
+const DropDownHeader = styled("a")`
+  position: relative;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 18px;
+  text-align: center;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  text-decoration: none;
+`;
+
+const DropDownListContainer = styled("div")``;
+
+const DropDownList = styled("ul")`
+  position: absolute;
+  margin-top: 0.125rem;
+  background-color: #2f2f2f;
+  text-align: center;
+  z-index: 1000;
+  min-width: 10rem;
+  padding: 0.5rem 0;
+  margin: 0;
+  font-size: 1rem;
+  color: #212529;
+  list-style: none;
+  background-clip: padding-box;
+  border: 1pxsolidrgba (0, 0, 0, 0.15);
+  border-radius: 0.25rem;
+`;
+
+const ListItem = styled("li")`
+  display: block;
+  padding: 0.25rem 1rem;
+  clear: both;
+  font-weight: 400;
+  color: #fff;
+  text-align: inherit;
+  text-decoration: none;
+  white-space: nowrap;
+  background-color: transparent;
+  border: 0;
+`;
+
+const NavLinks = (props) => {
+  const [active, setActive] = useState("navItem1");
+  const [isOpen, setIsOpen] = useState(false);
+  const toggling = () => setIsOpen(!isOpen);
+  return (
     <>
-        <li className='mx-5 mt-4' onClick={() => setActive("navItem1") && props.isMobile && props.closeMobileMenu()}>
-        <NavLink activeClassName="active" to='/nftcollection'>NFT <br/> COLLECTIONS</NavLink>
-        </li>
-        <li className='mx-5 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
-          <NavLink activeClassName="active" to='/claimqubes'>CLAIM <br /> QUBES</NavLink>
-        </li>
-        <li className='mx-5 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
-          <NavLink activeClassName="active" to='/mycollection'>MY <br /> COLLECTION</NavLink>
-        </li>
-        {/* <li className='mx-5 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        className="mx-5 mt-4"
+        onClick={() =>
+          setActive("navItem1") && props.isMobile && props.closeMobileMenu()
+        }
+      >
+        <NavLink activeClassName="active" to="/nftcollection">
+          NFT <br /> COLLECTIONS
+        </NavLink>
+      </li>
+      <li
+        className="mx-5 mt-4"
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <NavLink activeClassName="active" to="/claimqubes">
+          CLAIM <br /> QUBES
+        </NavLink>
+      </li>
+      <li
+        className="mx-5 mt-4"
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <NavLink activeClassName="active" to="/mycollection">
+          MY <br /> COLLECTION
+        </NavLink>
+      </li>
+      {/* <li className='mx-5 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
           <a href="#" key={'homeqube'} target="_blank" rel="noreferrer">
           <NavLink activeClassName="active" to='/faqs'> FAQS </NavLink>
            
           </a>
         </li> */}
-       
-        {/* <li className='mx-3'>
+
+      {/* <li className='mx-3'>
           <Link to='/marketplace'>Marketplace</Link>
         </li> */}
-        {/* <li className='mx-5 mt-4'>
+      {/* <li className='mx-5 mt-4'>
           <Link to='/sell'>SELL</Link>
         </li> */}
-        <li className='mx-5 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
-          <a href="https://www.homeqube.ai/" key={'homeqube'} target="_blank" rel="noreferrer">
+      {/* <li
+        className="mx-5 mt-4"
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <a
+          href="https://www.homeqube.ai/"
+          key={"homeqube"}
+          target="_blank"
+          rel="noreferrer"
+        >
           BETA
-          </a>
-        </li>
-         {/* <li className='mx-5 mt-4'>
+        </a>
+      </li> */}
+      <DropDownContainer className="mx-5 mt-4">
+        <DropDownHeader onClick={toggling} className="text-center dropdown ">
+          LEGAL
+        </DropDownHeader>
+        {isOpen && (
+          <DropDownList className="dropdown-list">
+            <ListItem>
+              <NavLink to="/terms" activeClassName="active" className="ahref">
+                Terms of Use
+              </NavLink>
+            </ListItem>
+            <ListItem>
+              <NavLink to="/policy" activeClassName="active" className="ahref">
+                Privacy Policy
+              </NavLink>
+            </ListItem>
+          </DropDownList>
+        )}
+      </DropDownContainer>
+      {/* <li className='mx-5 mt-4'>
           <a href="https://www.qube.homeqube.com/" key={'qube'} target="_blank" rel="noreferrer">
           ICO <br /> WEBISTE
           </a>
         </li> */}
-        <li className='mx-3 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="https://t.me/homeqube" key={'telegram'} target="_blank" rel="noreferrer">
-          <button className="exchange-button-more btn rounded-pill" type="button">
+      <li
+        className="mx-3 mt-4"
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <a
+          href="https://t.me/homeqube"
+          key={"telegram"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button
+            className="exchange-button-more btn rounded-pill"
+            type="button"
+          >
             JOIN TELEGRAM
           </button>
         </a>
-        </li>
-        <li className='mx-3 mt-4' onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="https://discord.gg/JhQXmjm59e" key={'telegram'} target="_blank" rel="noreferrer">
-          <button className="exchange-button-more2 btn rounded-pill" type="button">
+      </li>
+      <li
+        className="mx-3 mt-4"
+        onClick={() => props.isMobile && props.closeMobileMenu()}
+      >
+        <a
+          href="https://discord.gg/JhQXmjm59e"
+          key={"telegram"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button
+            className="exchange-button-more2 btn rounded-pill"
+            type="button"
+          >
             JOIN DISCORD
           </button>
         </a>
-        </li>
-        {/* <DropdownAnchor ref={anchorRef} onClick={handleToggle}>
+      </li>
+
+      {/* <DropdownAnchor ref={anchorRef} onClick={handleToggle}>
           Other Layouts
           <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
             {({ TransitionProps, placement }) => (
@@ -80,10 +196,9 @@ const NavLinks = (props) =>{
             )}
           </Popper>
         </DropdownAnchor> */}
-      
-      
-      </>);
-}
+    </>
+  );
+};
 export default NavLinks;
 
 const HeaderBar = styled.div`
@@ -93,13 +208,13 @@ const HeaderBar = styled.div`
   align-items: center;
   justify-content: center;
   margin-bottom: 30px;
-`
+`;
 
 const DropdownAnchor = styled.li`
   cursor: pointer;
 
   &:hover {
-    color: rgb(131,146,161);
+    color: rgb(131, 146, 161);
   }
 
   > div {
@@ -119,7 +234,7 @@ const DropdownAnchor = styled.li`
       }
     }
   }
-`
+`;
 
 // const WalletAmount = styled.div`
 //   color: black;
@@ -153,7 +268,7 @@ const Wallet = styled.ul`
   flex: 0 0 auto;
   margin: 0;
   padding: 0;
-`
+`;
 
 const ConnectButton = styled(WalletMultiButton)`
   border-radius: 18px !important;
@@ -161,7 +276,7 @@ const ConnectButton = styled(WalletMultiButton)`
   background-color: #4e44ce;
   margin: 0 auto;
   margin-top: 1.5rem !important;
-`
+`;
 
 const Logo = styled.div`
   flex: 0 0 auto;
@@ -170,7 +285,7 @@ const Logo = styled.div`
   img {
     height: 7rem;
   }
-`
+`;
 
 const Menu = styled.ul`
   list-style: none;
@@ -204,4 +319,4 @@ const Menu = styled.ul`
       border-bottom: 4px solid var(--title-text-color);
     }
   }
-`
+`;
