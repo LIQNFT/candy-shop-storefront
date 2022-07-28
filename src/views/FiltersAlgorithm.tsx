24412b7e9fc41642cba073a7254cbac1364e3d0a
jdojade <jade.delima.oloroso@gmail.com>
2022-07-28 12:10:54 +0800

    update algorithm

--- src/views/FiltersAlgorithm.tsx
+++ src/views/FiltersAlgorithm.tsx
@@ -64,7 +64,7 @@ export default AlgorithmFilter;
 
 const FILTERS = [
   {
-    name: "Marine Leisure",
+    name: "Algorithms",
     collectionId: "1",
     identifier: [-45564651],
     qubeClaims: "",
