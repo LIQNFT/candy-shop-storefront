1a4aff8603e5493b0ec4d808bf884073296e5971
tvminh <van-minh.tran@spiritlabs.co>
2022-08-10 20:46:08 +0700

    bug: get invalid symbol from localStorage

--- src/components/Currency.tsx
+++ src/components/Currency.tsx
@@ -1,33 +1,36 @@
-import { useContext, createContext, useState, useEffect } from 'react';
+import { useContext, createContext, useState, useEffect } from "react";
 
 interface CurrencyType {
   currency: string;
   setCurrency: Function;
   getCurrencySettings: Function;
   currencyOptions: {
-    currencySymbol: string,
-    treasuryMint: string,
-    currencyDecimals: number,
-    priceDecimals: number,
-    volumeDecimals: number
+    currencySymbol: string;
+    treasuryMint: string;
+    currencyDecimals: number;
+    priceDecimals: number;
+    volumeDecimals: number;
   }[];
 }
 
 const CurrencyContext = createContext<CurrencyType>({
-  currency: 'SOL',
+  currency: "SOL",
   setCurrency: () => {},
   getCurrencySettings: () => {},
-  currencyOptions: [{
-    currencySymbol: 'SOL',
-    treasuryMint: 'So11111111111111111111111111111111111111112',
-    currencyDecimals: 9,
-    priceDecimals: 3,
-    volumeDecimals: 1
-  }],
+  currencyOptions: [
+    {
+      currencySymbol: "SOL",
+      treasuryMint: "So11111111111111111111111111111111111111112",
+      currencyDecimals: 9,
+      priceDecimals: 3,
+      volumeDecimals: 1,
+    },
+  ],
 });
 
 // create local storage key from creator wallet address
-const CANDY_SHOP_CURRENCY_SYMBOL = 'CandyShop' + (process.env.REACT_APP_CANDY_SHOP_CREATOR_ADDRESS!).slice(0, 8);
+const CANDY_SHOP_CURRENCY_SYMBOL =
+  "CandyShop" + process.env.REACT_APP_CANDY_SHOP_CREATOR_ADDRESS!.slice(0, 8);
 
 export function useCurrency() {
   return useContext(CurrencyContext);
@@ -35,34 +38,38 @@ export function useCurrency() {
 
 export function CurrencyProvider({
   currencyOptions,
-  children
+  children,
 }: {
-  currencyOptions: CurrencyType['currencyOptions'],
-  children: any
+  currencyOptions: CurrencyType["currencyOptions"];
+  children: any;
 }) {
-  // users' preferred currency is stored and retrieved from local storage
-  let defaultSymbol = localStorage.getItem(CANDY_SHOP_CURRENCY_SYMBOL) || currencyOptions[0].currencySymbol;
-
-  const [currency, setCurrency] = useState(defaultSymbol);
+  const [currency, setCurrency] = useState(() => {
+    const localSymbol = localStorage.getItem(CANDY_SHOP_CURRENCY_SYMBOL);
+    const currency = currencyOptions.find(
+      (currency) => currency.currencySymbol === localSymbol
+    );
+    if (currency) return currency.currencySymbol;
+    return currencyOptions[0].currencySymbol;
+  });
 
   useEffect(() => {
     localStorage.setItem(CANDY_SHOP_CURRENCY_SYMBOL, currency);
   }, [currency]);
 
   const getCurrencySettings = () => {
-    return currencyOptions.find(option => currency === option.currencySymbol);
+    return currencyOptions.find((option) => currency === option.currencySymbol);
   };
 
   const value = {
     currency,
     setCurrency,
     getCurrencySettings,
-    currencyOptions
+    currencyOptions,
   };
 
   return (
     <CurrencyContext.Provider value={value}>
       {children}
     </CurrencyContext.Provider>
-  )
+  );
 }
