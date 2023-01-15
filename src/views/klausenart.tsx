57f706fab8ee86933c03da9cf74da400bb3bf09f
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-01-15 02:19:42 +0100

    Update klausenart.tsx

--- src/views/klausenart.tsx
+++ src/views/klausenart.tsx
@@ -1,1 +1,18 @@
+import styled from 'styled-components';
+import { Orders, Stat } from '@liqnft/candy-shop';
+import { ConnectButton } from '@/components/ConnectButton';
+import { useUserWallet } from '@/hooks/useUserWallet';
+import { useShopStore } from '@/store/useShopStore';
 
+const Marketplace: React.FC = () => {
+
+    <DesContainer>
+
+      <h1> Klausenart</h1>
+      
+    </DesContainer>
+};
+
+const DesContainer = styled.div`
+  width: 100%;
+`;
