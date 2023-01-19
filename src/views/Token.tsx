8e3849bcd7deeff81de0885d5293f44831840cbd
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-01-19 22:19:31 +0100

    Update Token.tsx

--- src/views/Token.tsx
+++ src/views/Token.tsx
@@ -22,7 +22,7 @@ const Token: React.FC = () => {
         candyShop={candyShop}
         walletConnectComponent={<ConnectButton />}
         filters={FILTERS}
-        shopFilters = {false}
+        shopFilters = {true}
       />
     </DesContainer>
   );
