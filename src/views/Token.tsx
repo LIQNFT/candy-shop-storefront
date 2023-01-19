19a1116a550592b6b53d98ea29d27424621e193e
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-01-19 21:32:32 +0100

    Create Token.tsx

---
+++ src/views/Token.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { Orders, Stat } from '@liqnft/candy-shop';
+import { ConnectButton } from '@/components/ConnectButton';
+import { useUserWallet } from '@/hooks/useUserWallet';
+import { useShopStore } from '@/store/useShopStore';
+
+const Token: React.FC = () => {
+  const userWallet = useUserWallet();
+  const candyShop = useShopStore((s) => s.candyShop);
+  if (!candyShop) return null;
+
+  return (
+    <DesContainer>
+      <Stat
+        candyShop={candyShop}
+        title={'Marketplace'}
+        description={'Enable users to filter by collections and search for NFTs.'}
+        style={{ paddingBottom: 50 }}
+      />
+      <Orders
+        wallet={userWallet}
+        candyShop={candyShop}
+        walletConnectComponent={<ConnectButton />}
+        filters={FILTERS}
+      />
+    </DesContainer>
+  );
+};
+
+export default Token;
+
+const FILTERS = [
+  { name: 'Shibas', collectionId: '2', identifier: 1235887132 },
+];
+
+const DesContainer = styled.div`
+  width: 100%;
+  .candy-filter {
+    color: #fff;
+    li:hover {
+      color: #7522f5;
+    }
+    .candy-search input {
+      padding: 10px 4px 10px 24px;
+      width: 100%;
+    }
+  }
+`;
