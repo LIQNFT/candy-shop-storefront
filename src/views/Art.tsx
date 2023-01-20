82594c0659209a594dc7cce7d30627f23fb1db02
klausenArt <122472841+klausenart@users.noreply.github.com>
2023-01-20 11:39:14 +0100

    Update Art.tsx

--- src/views/Art.tsx
+++ src/views/Art.tsx
@@ -13,8 +13,8 @@ const Art: React.FC = () => {
     <DesContainer>
       <Stat
         candyShop={candyShop}
-        title={'Marketplace'}
-        description={'Enable users to filter by collections and search for NFTs.'}
+        title={'Collectibles'}
+        description={'With our digital collectibles become a premium digital art collector.'}
         style={{ paddingBottom: 50 }}
       />
       <Orders
@@ -23,7 +23,6 @@ const Art: React.FC = () => {
         walletConnectComponent={<ConnectButton />}
         filters={FILTERS}
         shopFilters = {false}
-        search
         
 
         
